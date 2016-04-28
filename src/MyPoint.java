423075797c21522b45f4f21da9f417032647d088
노요셉 <@YOBS>
2016-04-28 18:44:57 +0900

    revise of code at month

--- src/MyPoint.java
+++ src/MyPoint.java
@@ -58,7 +58,7 @@ public class MyPoint {
 		
 		Calendar now = Calendar.getInstance();
 		System.out.println(now.get(Calendar.YEAR));
-		System.out.println(now.get(Calendar.MONTH));
+		System.out.println(now.get(Calendar.MONTH)+1);
 		System.out.println(now.get(Calendar.DAY_OF_MONTH));
 		System.out.println(now.get(Calendar.HOUR));
 		System.out.println(now.get(Calendar.MINUTE));
